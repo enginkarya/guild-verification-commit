683da82d6ffdf6fbdbb52cc9167a08ed2f33a06d
enginkarya <enginkarya@gmail.com>
2025-10-31 15:03:12 +0400

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T11:03:13.838Z
-Commit: Add multi-language support
+Timestamp: 2025-10-31T11:03:15.542Z
+Commit: Resolve null pointer exception in user service
